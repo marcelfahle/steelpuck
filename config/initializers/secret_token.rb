e212a7fc38e1dd5d997421722ad83e51d2d99121
Marcel Fahle <m.fahle@gmail.com>
2012-11-01 08:46:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tinypuck::Application.config.secret_token = 'ba469b3fdf94a857733f6d43d0ccfc38962fea9caa433f958ab5c622418659bce0679571334a6bdf676fbdeee240f9e8ca233d051563087b224c2a0877d8f659'
